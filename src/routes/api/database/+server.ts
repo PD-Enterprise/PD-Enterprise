349092ca89522ad56d252524f18d10d8366895cc
PD <pdenterprise314@gmail.com>
2024-12-25 20:20:52 +0530

    Update +server.ts

--- src/routes/api/database/+server.ts
+++ src/routes/api/database/+server.ts
@@ -8,6 +8,7 @@ export const POST: RequestHandler = async ({ request }) => {
     if (query[0]) {
         const headers = newCookie('Session_id');
         const cookieID = headers['Set-cookie'].split('=')[1].split(';')[0];
+
         const updateSession = await sql`update users set session_id = ${cookieID} where session_id = ${body.session_id}`;
         return json({ status: 200, data: headers, message: 'success' }, { headers: headers });
     } else {
